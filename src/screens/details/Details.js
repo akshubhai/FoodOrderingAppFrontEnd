57a6d298a928fe43676d61c7c22c65ef7173a959
ms530 <sharmamukul2596@gmail.com>
2021-05-23 19:24:38 +0530

    Method to get index of item from menu

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -80,4 +80,14 @@ class Details extends Component {
         xhr.send(data);
     }
 
+    //Function to get the index of the item
+    getIndex = (value, arr, prop) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i][prop] === value) {
+                return i;
+            }
+        }
+        return -1; //to handle the case where the value doesn't exist
+    }
+
     
