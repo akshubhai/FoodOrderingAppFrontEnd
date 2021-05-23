5e0997c25cf529e7bc5d402056e73850d9bb96c0
ms530 <sharmamukul2596@gmail.com>
2021-05-23 18:44:42 +0530

    Importing header and other styling component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+//importing the header component
+import Header from '../../common/header/Header';
+
+//importing material-ui components
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+//importing font awesome
+import '../../../node_modules/font-awesome/css/font-awesome.min.css';
+
+//importing the css file of the Home page
+import './Home.css';
+
